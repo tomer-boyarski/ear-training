8f0a8d86ced9ffa3de9c2a941be9b5b94ae6212b
tomer boyarski <tomer.boyarski@gmail.com>
2022-08-02 17:00:29 +0300

    plot with datetime

--- main.py
+++ main.py
@@ -196,55 +196,6 @@ class Iteration:
             return output
 
         @staticmethod
-        def raise_exceptions_for_notes_and_intervals():
-            # Check that all note attributes are valid
-            note_attributes = {'names': config.note_names,
-                               'numbers': config.note_numbers,
-                               'indices': config.note_indices}
-            note_attributes = {k: v for k, v in note_attributes.items() if v is not None}
-
-            # check that all note attributes are valid
-            for key_1, list_1 in note_attributes.items():
-                if config.key == 'WHITE':
-                    att_list = getattr(constants.white_note, key_1)
-                    for item in list_1:
-                        if item is not None and item not in att_list:
-                            raise Exception('invalid note ' + key_1)
-                elif config.key != 'WHITE':
-                    raise Exception('missing exception rule for non-white keys')
-
-            # Check that all note attributes have the same length
-            for key_1, list_1 in note_attributes.items():
-                for key_2, list_2 in note_attributes.items():
-                    if len(list_1) != len(list_2):
-                        raise Exception('Length of ' + key_1 + ' is ' + str(len(list_1)) +
-                                        ' but ' +
-                                        ' length of ' + key_2 + ' is ' + str(len(list_2)) +
-                                        '. They should be the same.')
-
-            # Check that the length of all note attributes is the same as the number of notes
-            if config.number_of_notes is not None:
-                for config.key, a_list in note_attributes.items():
-                    if len(a_list) != config.number_of_notes:
-                        raise Exception('length of ' + config.key + ' is ' + str(len(a_list)) +
-                                        ' but number of notes is ' + str(config.number_of_notes) +
-                                        '. They should be the same.')
-
-            # Check that the length of all note attributes is the same as number of intervals plus one
-            if config.intervals is not None:
-                for config.key, a_list in note_attributes.items():
-                    if len(a_list) != len(config.intervals) + 1:
-                        raise Exception('Length of ' + config.key + ' is ' + str(len(a_list)) +
-                                        ' but length of "interval" is ' + str(len(config.intervals)) +
-                                        '. Length of "intervals" should be equal to length of ' +
-                                        config.key + ' - 1.')
-
-            if config.step_size is not None or config.step_trend is not None:
-                for key_1, list_1 in note_attributes.items():
-                    if list_1[0] is not None:
-                        raise Exception('bottom note is over-defined')
-
-        @staticmethod
         def get_number_of_notes_intervals_and_step_size_from_literals(
                 iteration_list, note_indices=None):
             number_of_notes = None
@@ -279,125 +230,6 @@ class Iteration:
                         step_size = abs(step)
                         step_trend = np.sign(step_size)
             return number_of_notes, intervals, step_size, step_trend
-
-        def __init__(self, iteration_list, phase, previous_note_indices,
-                     mistakes_counter, note_indices=None):
-            self.raise_exceptions_for_notes_and_intervals()
-
-            self.max_level = constants.max_level_func(
-                keys=config.keys, step_size=None, intervals=None,
-                number_of_notes=None, step_size_level=None,
-                number_of_notes_level=None, interval_level=None)
-
-            self.level = self.Level_Step_and_Intervals(iteration_list=iteration_list,
-                                    max_level=self.max_level,
-                                    phase=phase, mistakes_counter=mistakes_counter)
-
-            number_of_notes, _, _, _ = \
-                self.get_number_of_notes_intervals_and_step_size_from_literals(
-                    iteration_list=iteration_list, note_indices=note_indices)
-
-            if constants.show_phase:
-                print('phase = ' + phase)
-            # self.size = None
-            self.step = None
-            # self.notes = self.Note(names=note_names, numbers=note_numbers, indices=note_indices)
-            self.notes = []
-            self.number_of_notes = number_of_notes
-
-            if number_of_notes is None:
-                self.number_of_notes = np.random.choice(
-                    np.arange(1, 1 + constants.max_chord_size), 1,
-                    p=constants.levels.number_of_notes[self.level.number_of_notes, :])
-                self.number_of_notes = self.number_of_notes[0]
-
-            intervals = self.create_none_list_if_necessary(list_or_none=config.intervals, size=self.number_of_notes - 1)
-            note_names = self.create_none_list_if_necessary(list_or_none=config.note_names, size=self.number_of_notes)
-            note_numbers = self.create_none_list_if_necessary(list_or_none=config.note_numbers, size=self.number_of_notes)
-            if note_indices is None:
-                note_indices = config.note_indices
-            note_indices = self.create_none_list_if_necessary(list_or_none=note_indices, size=self.number_of_notes)
-
-            while True:
-                for i in range(self.number_of_notes):
-                    if i == 0:
-                        self.set_chord_step_and_bottom_note(previous_note_indices=previous_note_indices, i=i,
-                                                            note_names=note_names, note_numbers=note_numbers,
-                                                            note_indices=note_indices,
-                                                            level=self.level)
-                    elif i >= 1:
-                        self.set_interval_and_non_bottom_note(
-                            index_in_chord=i,
-                            note_names=note_names, note_numbers=note_numbers, note_indices=note_indices,
-                            intervals=intervals,
-                            level=self.level)
-                indices = [note.index for note in self.notes]
-                if all(index in constants.keys.white.indices for index in indices):
-                    break
-                else:
-                    self.notes = []
-                    pass
-
-        def set_chord_step_and_bottom_note(
-                self, previous_note_indices, i,
-                note_names=None, note_numbers=None, note_indices=None,
-                step_trend=None, step_size=None,
-                level=None):
-            keys = config.keys
-            note_is_literally_defined = \
-                note_indices[i] is not None or \
-                note_names[i] is not None or \
-                note_numbers[i] is not None
-            step_is_literally_defined = step_size is not None or step_trend is not None
-            if note_is_literally_defined:
-                # if not step_is_literally_defined:
-                self.notes.append(self.Note(keys=keys, index=note_indices[i],
-                                            number=note_numbers[i],
-                                            name=note_names[i]))
-                self.step = self.Step(keys=keys,
-                                 current_index=note_indices[i],
-                                 previous_index=previous_note_indices[i])
-            else:
-                while True:
-                    if step_is_literally_defined:
-                        self.step = self.Step(keys=keys, size=step_size,
-                                         trend=step_trend)
-                    else:
-                        self.step = self.Step(keys=keys, difficulty_level=level.step_size)
-                    new_bottom_index = previous_note_indices[0] + \
-                                       (self.step.size - 1) * self.step.trend
-                    if 0 <= new_bottom_index < len(constants.keys.white.numbers) / 2:
-                        self.notes.append(self.Note(keys=keys, index=new_bottom_index))
-                        break
-
-        def set_interval_and_non_bottom_note(
-                self, index_in_chord,
-                note_names=None, note_numbers=None, note_indices=None,
-                intervals=None,
-                level=None):
-            keys = config.keys
-            note_is_literally_defined = \
-                note_indices[index_in_chord] is not None or \
-                note_names[index_in_chord] is not None or \
-                note_numbers[index_in_chord] is not None
-            if note_is_literally_defined:
-                # if intervals[index_in_chord - 1] is None:
-                self.notes.append(self.Note(keys=keys, index=note_indices[index_in_chord],
-                                            number=note_numbers[index_in_chord],
-                                            name=note_names[index_in_chord]))
-                intervals[index_in_chord - 1] = self.notes[-1].index - self.notes[-2].index + 1
-            else:
-                if intervals[index_in_chord - 1] is None:
-                    p = getattr(constants.levels.intervals, keys)
-                    p = p[level.intervals, :]
-                    x = getattr(constants.keys, keys)
-                    x = len(x.names) + 2
-                    intervals[index_in_chord - 1] = np.random.choice(
-                        np.arange(2, x), 1, p=p)
-                lower_note = self.notes[index_in_chord - 1].index
-                note_index = lower_note + intervals[index_in_chord - 1] - 1
-                note_index = note_index[0]
-                self.notes.append(self.Note(keys=keys, index=note_index))
 
     class Question_Steps_and_Intervals(Question):
         class Level:
@@ -670,22 +502,6 @@ class Iteration:
                 # self.index = np.in1d(
                 #     constants.note_numbers_C_to_C,
                 #     self.number).nonzero()[0]
-
-        @staticmethod
-        def create_none_list_if_necessary(list_or_none, size):
-            if list_or_none is None:
-                output = [None] * size
-            else:
-                if type(list_or_none) is list or type(list_or_none) is np.ndarray:
-                    if len(list_or_none) == size:
-                        output = list_or_none
-                    else: # elif len(list_or_none) != size:
-                        raise Exception('The list received by "create_None_list_if_necessary" ' +
-                                        str(list_or_none) + ' has a length of ' + str(len(list_or_none)) +
-                                        ' but it should have a length of ' + str(size))
-                else: # elif type(list_or_none) is not list and type(list_or_none) is not np.array:
-                    raise Exception('note attributes inputs must be lists or numpy arrays')
-            return output
 
         @staticmethod
         def raise_exceptions_for_notes_and_intervals():
@@ -1134,10 +950,11 @@ def main():
     else:
         iteration_list = iteration_list[:-1]
         file_name = 'users\\' + config.user + '\\iteration_list.pkl'
-        if os.path.isfile(file_name): 
+        # if os.path.isfile(file_name): 
+        try:
             with open(file_name, 'rb') as input:
                 previous_iteration_list = pickle.load(input)
-        else:
+        except:
             previous_iteration_list = []
         iteration_list = previous_iteration_list + iteration_list
         with open(file_name, 'wb') as output:
